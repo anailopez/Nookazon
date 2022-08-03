e5091efeacd18d6d1ab3dc59b09a55fae93c0dcc
Anai Lopez <anaijocelyn@gmail.com>
2022-08-03 08:22:22 -0700

    updating heroku

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     Maple = User(
-        username='Maple', icon='https://static.wikia.nocookie.net/animalcrossing/images/a/a2/Maple_NH.png/revision/latest?cb=20200720220654', email='maple@nookmail.com', address='123 Island Way', hashed_password='password')
+        username='Maple', icon='https://static.wikia.nocookie.net/animalcrossing/images/a/a2/Maple_NH.png/revision/latest?cb=20200720220654', email='maple@nookmail.com', address='123 Island Way', password='password')
     Puddles = User(
-        username='Puddles', icon='https://static.wikia.nocookie.net/animalcrossing/images/f/f9/Puddles_NH.png/revision/latest?cb=20200802145129', email='puddles@nookmail.com', address='222 Island Ave', hashed_password='password')
+        username='Puddles', icon='https://static.wikia.nocookie.net/animalcrossing/images/f/f9/Puddles_NH.png/revision/latest?cb=20200802145129', email='puddles@nookmail.com', address='222 Island Ave', password='password')
     Teddy = User(
-        username='Teddy', icon='https://static.wikia.nocookie.net/animalcrossing/images/b/b9/Teddy_NH.png/revision/latest?cb=20200802072530', email='teddy@nookmail.com', address='456 Island Rd', hashed_password='password')
+        username='Teddy', icon='https://static.wikia.nocookie.net/animalcrossing/images/b/b9/Teddy_NH.png/revision/latest?cb=20200802072530', email='teddy@nookmail.com', address='456 Island Rd', password='password')
 
     db.session.add(Maple)
     db.session.add(Puddles)
