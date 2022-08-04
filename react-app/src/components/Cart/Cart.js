4fd50213e02cac95114f07c069daaa5a1cd27e2f
Anai Lopez <anaijocelyn@gmail.com>
2022-08-04 16:34:08 -0700

    remove item from cart functional

--- react-app/src/components/Cart/Cart.js
+++ react-app/src/components/Cart/Cart.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import './cart.css'
 
 
 const Cart = () => {
     const userId = useSelector(state => state.session.user.id)
 
     let [cart, setCart] = useState([]);
+    const [itemId, setItemId] = useState(null);
     let savedCart = null;
 
     if (userId) {
@@ -25,24 +28,44 @@ const Cart = () => {
     //     //todo
     // }
 
-    // const removeItemFromCart = (itemId) => {
-    //     //todo
-    // }
+    const removeItem = (itemId) => {
+        //create a copy of cart so state isn't overwritten!
+        let cartCopy = [...cart];
+
+        cartCopy = cartCopy.filter(cartItem => cartItem.item.id !== itemId);
+
+        setCart(cartCopy);
+        localStorage.setItem(userId, JSON.stringify(cartCopy));
+    }
+
+    const handleDelete = (e) => {
+        e.preventDefault();
+        removeItem(itemId)
+    }
 
     return (
         <div>
             <h1>Shopping Cart</h1>
             {cart && cart.map(cartItem => (
-                <div className='cart-item'>
-                    <img src={cartItem.item.image} />
+                <div className='cart-item' key={cartItem.item.id}>
+                    <Link to={`/items/${cartItem.item.id}`}>
+                        <img src={cartItem.item.image} />
+                    </Link>
                     <h2>{cartItem.item.title}</h2>
                     <h3>{cartItem.item.price} bells</h3>
-                    
+                    <p>Qty: {cartItem.quantity}</p>
+                    <form onSubmit={handleDelete}>
+                        <button onClick={() => setItemId(cartItem.item.id)}>Delete</button>
+                    </form>
                 </div>
             ))}
             {cart && !cart.length > 0 && (
                 <p>Your cart is empty!</p>
             )}
+            <div>
+                <h2>Subtotal</h2>
+                <button>Proceed to checkout</button>
+            </div>
         </div>
     )
 }
