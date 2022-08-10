a3ae34a306f68b7cc9be603373d79af02d7aa428
Anai Lopez <anaijocelyn@gmail.com>
2022-08-10 11:30:11 -0700

    order details page styling

--- react-app/src/components/OrderDetails/OrderDetails.js
+++ react-app/src/components/OrderDetails/OrderDetails.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { thunkGetSingleOrder } from "../../store/orders";
 import { thunkEditOrder } from "../../store/orders";
 import Modal from 'react-modal';
@@ -48,13 +48,13 @@ const OrderDetails = () => {
     return (
         <div className="order-details">
             {order && (
-                <>
+                <div className="order-details-content">
                     <h1>Order Details</h1>
                     <div className="basic-info" id='main'>
-                        {/* <div> */}
-                        <p>Ordered on *DATE HERE*</p>
-                        <p>Order # {orderId}</p>
-                        {/* </div> */}
+                        <div id='order-basic-info'>
+                            <p id='first-p'>Ordered on {order.order_date}</p>
+                            <p id='second-p'>Order # {orderId}</p>
+                        </div>
                         <div>
                             <Modal isOpen={showEditForm} style={customStyles}>
                                 <form>
@@ -67,18 +67,19 @@ const OrderDetails = () => {
                                 <button onClick={() => { dispatch(thunkEditOrder(order.id, deliveryInfo)); closeEditModal() }}>Update instructions</button>
                                 <button onClick={closeEditModal}>Cancel</button>
                             </Modal>
-                            <button onClick={openEditModal}>Edit delivery instructions</button>
+                            <button id='edit-delivery-btn' onClick={openEditModal}>Edit delivery instructions</button>
                         </div>
                     </div>
 
                     <div className="details">
                         <div>
                             <h3>Shipping Address</h3>
-                            <p>{user.address}</p>
+                            <p>{user.street_address}</p>
+                            <p>{user.town_name}</p>
                         </div>
                         <div>
                             <h3>Payment Method</h3>
-                            <p>*payment method here*</p>
+                            <p><i class="fa-solid fa-credit-card" /> ending in {user.payment_method}</p>
                         </div>
                         <div>
                             <h3>Delivery Instructions</h3>
@@ -96,21 +97,31 @@ const OrderDetails = () => {
                         </div>
                         {order && order.items.map(orderItem => (
                             <div key={orderItem.id} className='orderDetails-item'>
+                                {/* <p id='date'>{order.delivery_date}</p> */}
                                 <div className="image-buyagain">
-                                    <img src={orderItem.item.image} alt="orderItem img" />
+                                    <Link to={`/items/${orderItem.item.id}`}>
+                                        <img src={orderItem.item.image} alt="orderItem img" />
+                                    </Link>
                                     <div id='buy-again'>
-                                        <p>{orderItem.item.title}</p>
-                                        <p>{orderItem.item.price} bells</p>
-                                        <button>Buy it again</button>
+                                        <Link to={`/items/${orderItem.item.id}`}>
+                                            <p id='p-item-link'>{orderItem.item.title}</p>
+                                        </Link>
+                                        <p id='orderitem-price'>{orderItem.item.price} bells</p>
+                                        <p>Qty: {orderItem.quantity}</p>
+                                        <Link to={`/items/${orderItem.item.id}`}>
+                                            <button id='buy-again-btn'><i class="fa-solid fa-bag-shopping" />Buy it again</button>
+                                        </Link>
                                     </div>
                                 </div>
                                 <div>
-                                    <button>Write a product review</button>
+                                    <Link to={`/create-review/${orderItem.item.id}`}>
+                                        <button className="details-review-btn" id='order-review-btn'>Write a product review</button>
+                                    </Link>
                                 </div>
                             </div>
                         ))}
                     </div>
-                </>
+                </div>
             )}
         </div>
 
